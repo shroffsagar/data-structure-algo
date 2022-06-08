b2cad039e8ab7a3dbf2b17688275f8604bfa7a82
Sagar Shroff <shroffsagar91@gmail.com>
2022-06-08 21:37:08 +0530

    Added Valid_Parentheses program.

---
+++ src/main/java/Valid_Parentheses.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+//https://leetcode.com/problems/valid-parentheses/
+public class Valid_Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> brackets = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(!brackets.isEmpty() && c == ')' && brackets.peek() == '(')
+                brackets.pop();
+            else if(!brackets.isEmpty() && c == ']' && brackets.peek() == '[')
+                brackets.pop();
+            else if(!brackets.isEmpty() && c == '}' && brackets.peek() == '{')
+                brackets.pop();
+            else
+                brackets.push(c);
+        }
+        return brackets.isEmpty();
+    }
+}
