bfa468b52764ccfe1da06a786c1a7113fa5870b5
Sagar Shroff <shroffsagar91@gmail.com>
2022-06-12 14:40:52 +0530

    Inverting a binary tree using recursive.

---
+++ src/Invert_Binary_Tree.java
@@ -0,0 +1,27 @@
+package src;
+
+public class Invert_Binary_Tree {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
