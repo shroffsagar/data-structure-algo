880caa1ae23046c04ae58d193a3e055dd749475b
Sagar Shroff <shroffsagar91@gmail.com>
2022-06-12 14:57:45 +0530

    Finding maximum depth of a binary tree using recrusion.

--- src/Maximum_Depth_of_Binary_Tree.java
+++ src/Maximum_Depth_of_Binary_Tree.java
@@ -1,10 +1,11 @@
 package src;
 
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
 public class Maximum_Depth_of_Binary_Tree {
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
-        int leftDepth = maxDepth(root.left)+1;
-        int rightDepth = maxDepth(root.right)+1;
-        return (leftDepth>rightDepth) ? leftDepth : rightDepth;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Integer.max(leftDepth, rightDepth) + 1;
     }
 }
